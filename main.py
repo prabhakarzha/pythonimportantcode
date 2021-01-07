6cd162651876972e820b5f0810fcaa0312fc89c1
prabhakar kumar <70058887+prabhakarzha@users.noreply.github.com>
2021-01-07 20:26:25 +0530

    remove comment line

--- main.py
+++ main.py
@@ -40,46 +40,46 @@ print(list(filter_object))
 
 
 #--------------------------MAP------------------------------
-# numbers = ["3", "34", "64"]
-# numbers = list(map(int, numbers))
+numbers = ["3", "34", "64"]
+numbers = list(map(int, numbers))
 
-# for i in range(len(numbers)):
-#     numbers[i] = int(numbers[i])
+for i in range(len(numbers)):
+    numbers[i] = int(numbers[i])
 
-# numbers[2] = numbers[2] + 1
-# print(numbers[2])
+numbers[2] = numbers[2] + 1
+print(numbers[2])
 
-# def sq(a):
-#     return a*a
-#
-# num = [2,3,5,6,76,3,3,2]
-# square = list(map(sq, num))
-# print(square)
-# num = [2,3,5,6,76,3,3,2]
-# square = list(map(lambda x: x*x, num))
-# print(square)
+def sq(a):
+    return a*a
+
+num = [2,3,5,6,76,3,3,2]
+square = list(map(sq, num))
+print(square)
+num = [2,3,5,6,76,3,3,2]
+square = list(map(lambda x: x*x, num))
+print(square)
 
 
-# def square(a):
-#     return a*a
-#
-# def cube(a):
-#     return a*a*a
+def square(a):
+    return a*a
 
-# func = [square, cube]
-# num = [2,3,5,6,76,3,3,2]
-# for i in range(5):
-#     val = list(map(lambda x:x(i), func))
-#     print(val)
+def cube(a):
+    return a*a*a
+
+func = [square, cube]
+num = [2,3,5,6,76,3,3,2]
+for i in range(5):
+    val = list(map(lambda x:x(i), func))
+    print(val)
 
 #--------------------------FILTER------------------------------
-# list_1 = [1,2,3,4,5,6,7,8,9]
-#
-# def is_greater_5(num):
-#     return num>5
-#
-# gr_than_5 = list(filter(is_greater_5, list_1))
-# print(gr_than_5)
+list_1 = [1,2,3,4,5,6,7,8,9]
+
+def is_greater_5(num):
+    return num>5
+
+gr_than_5 = list(filter(is_greater_5, list_1))
+print(gr_than_5)
 #--------------------------REDUCE------------------------------
 from functools import reduce
 
